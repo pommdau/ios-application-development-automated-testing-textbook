7e44fd0dafdfd4bbe64163f08392c2a47925e92f
IKEH1024 <pondau1024@gmail.com>
2022-01-31 22:40:10 +0900

    until 3-3

--- 第3章/Chapter3_1/Chapter3_1Tests/Models/ExceptionTests.swift
+++ 第3章/Chapter3_1/Chapter3_1Tests/Models/ExceptionTests.swift
@@ -8,24 +8,82 @@
 import XCTest
 @testable import Chapter3_1
 
-class ExceptionTests: XCTestCase {
-    
-    // MARK: - Helpers
-    
-    enum DownloadError: Error {
+
+
+class AssertThrowTests: XCTestCase {
+        
+    // MARK: - Download Helpers
+
+    enum DownloadError: Error, Equatable {
         case connectionError
         case unknownError(code: Int)
     }
     
-    // MARK: - Tests
-    
-    func testXCTFailTests() {
-        
-        executeClosure(false) {
-            XCTFail("conditoinがfalseのときはクロージャが呼び出されないこと")
-            return // 後続の処理を実行したくない場合
+    func downloadContent(connectionFailed: Bool = false,
+                         unknownError: Bool = false) throws {
+        if connectionFailed {
+            throw DownloadError.connectionError
         }
         
+        if unknownError {
+            throw DownloadError.unknownError(code: 9)
+        }
     }
+    
+    // MARK: - Download Tests
+    
+    func testDownloadErrorThrowsError() {
+        // XCTAssertThrowsErrorの引数はスローされた例外
+        XCTAssertThrowsError(try downloadContent(connectionFailed: true, unknownError: false)) { (error: Error) -> Void in
+            XCTAssertEqual(error as? DownloadError, DownloadError.connectionError)
+        }
+        
+        XCTAssertThrowsError(try downloadContent(connectionFailed: false, unknownError: true)) { (error: Error) -> Void in
+            XCTAssertEqual(error as? DownloadError, DownloadError.unknownError(code: 9))
+        }
+        
+        XCTAssertThrowsError(try downloadContent(connectionFailed: false, unknownError: true)) { (error: Error) -> Void in
+            
+            guard let downloadError = error as? DownloadError else {
+                XCTFail()
+                return
+            }
+            
+            // switchを用いないAssociated Valueの取り出し方
+            // [Switch\-less extraction of enum associated values in Swift            ](https://medium.com/@marioboikov/switch-less-extraction-of-enum-associated-values-in-swift-58910b46cacd)
+            guard case .unknownError(let code) = downloadError else {
+                XCTFail()
+                return
+            }
+            
+            XCTAssertEqual(code, 9)
+        }
+    }
+    
+    // MARK: - SystemError Helpers
+    
+    struct SystemError: Error {
+        let message: String
+    }
+    
+    func throwSystemError() throws {
+        throw SystemError(message: "memory access error")
+    }
+    
+    func testSystemErrorThrowsError() {
+        XCTAssertThrowsError(try throwSystemError()) { (error: Error) -> Void in
+            
+            // SystemErrorにキャストできることの検証
+            guard let systemError = error as? SystemError else {
+                XCTFail()
+                return
+            }
+            
+            // messageプロパティの検証
+            XCTAssertEqual(systemError.message, "memory access error")
+            
+        }
+    }
+    
     
 }
